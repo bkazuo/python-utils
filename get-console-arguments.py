114327b7c9ddf5f71d1bb455fa8b2da193d245bb
Bruno Takahashi <bruno.kazuo90@gmail.com>
2017-04-12 11:21:34 -0300

    First commit

---
+++ get-console-arguments.py
@@ -0,0 +1,11 @@
+import sys
+
+# executing - python get-console-arguments.py Dolly
+
+def main():
+	print 'Hello there', sys.argv[1]
+	# Command line args are in sys.argv[1], sys.argv[2] ...
+    # sys.argv[0] is the script name itself and can be ignored
+
+if __name__ == '__main__':
+	main()
